c80e1359a8b9dd1672e6ffcc63548f910311eb7a
ArrowPC <battle.beater2005@gmail.com>
2023-09-08 17:02:33 +1000

    cleanup

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,15 +1,12 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/stl_bind.h>
-#include <opencv2/opencv.hpp>
 #include "palettum.h"
 
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(py_palettum_core)
 {
-    py::module handle("py_palettum_core", "docstring here");
+    py::module handle("py_palettum_core", "Core functionality for the Palettum project.");
 
     py::class_<Palettum>(handle, "Palettum")
         .def(py::init<py::array_t<uint8_t> &, const py::list &>())

388e85507976e082b76c83376ed57796982379dc
ArrowPC <battle.beater2005@gmail.com>
2023-09-13 12:43:36 +1000

    Refactored Delta-E computation unit test

--- tests/palettum_test.cpp
+++ tests/palettum_test.cpp
@@ -5,11 +5,11 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-TEST(DeltaE, 1)
+TEST(DeltaEComputation, TestSpecificLabValues)
 {
     cv::Vec3f lab1 = {50.0, 2.6772, -100.7751};
     cv::Vec3f lab2 = {50.0, 50.0, 89.7485};
-    EXPECT_EQ(Palettum::deltaE(lab1, lab2), 61.227918044407808);
+    EXPECT_NEAR(Palettum::deltaE(lab1, lab2), 61.227918044407808, 1e-9);
 }
 
 TEST(Image, jpeg)

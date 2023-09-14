44ecfe4ad93b6d1fd5922fb7bce0299aad56a371
ArrowPC <battle.beater2005@gmail.com>
2023-09-15 04:24:45 +1000

    make py v3.12 the minimum

--- setup.py
+++ setup.py
@@ -138,5 +138,5 @@ setup(
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
-    python_requires=">=3.10",
+    python_requires=">=3.12",
 )

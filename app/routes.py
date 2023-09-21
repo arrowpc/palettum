16ba5d2ec284eba017462d21212b2e30ee4b5757
ArrowPC <battle.beater2005@gmail.com>
2023-09-22 06:16:04 +1000

    refactor

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import jsonify, request, send_file
-from app import app
 import cv2
-import palettum
 import io
 import numpy as np
+import palettum
+from app import app
+
+VALID_IMAGE_PREFIX = "image/"
+RGB_RANGE = range(256)
 
 
 @app.errorhandler(ValueError)
@@ -21,6 +24,47 @@ def index():
     return "Welcome to Palettum API!"
 
 
+def validate_image_content_type(image):
+    if not image.content_type.startswith(VALID_IMAGE_PREFIX):
+        raise ValueError("Uploaded file is not an image")
+
+
+def parse_palette(palette_file):
+    palette_lines = palette_file.read().decode("utf-8").splitlines()
+    try:
+        palette = [eval(color) for color in palette_lines]
+        if not all(isinstance(color, tuple) and len(color) == 3 for color in palette):
+            raise ValueError
+    except (SyntaxError, ValueError):
+        raise ValueError("Invalid palette format. Please provide a valid RGB palette.")
+
+    for color in palette:
+        if not all(value in RGB_RANGE for value in color):
+            raise ValueError(
+                "Invalid RGB values in palette. Each value must be in the range [0, 255]."
+            )
+
+    return palette
+
+
+def resize_image(img, width, height):
+    original_height, original_width = img.shape[:2]
+
+    if width and not height:
+        aspect_ratio = original_height / original_width
+        height = int(aspect_ratio * width)
+    elif height and not width:
+        aspect_ratio = original_width / original_height
+        width = int(aspect_ratio * height)
+
+    if width and height:
+        if width <= 0 or height <= 0:
+            raise ValueError("Invalid dimensions after aspect ratio calculation.")
+        img = cv2.resize(img, (width, height))
+
+    return img
+
+
 @app.route("/upload", methods=["POST"])
 def upload_image():
     if "image" not in request.files:
@@ -31,22 +75,8 @@ def upload_image():
     image = request.files["image"]
     palette_file = request.files["palette"]
 
-    if not image.content_type.startswith("image/"):
-        raise ValueError("Uploaded file is not an image")
-
-    palette_lines = palette_file.read().decode("utf-8").splitlines()
-    try:
-        palette = [eval(color) for color in palette_lines]
-        if not all(isinstance(color, tuple) and len(color) == 3 for color in palette):
-            raise ValueError
-    except (SyntaxError, ValueError):
-        raise ValueError("Invalid palette format. Please provide a valid RGB palette.")
-
-    for color in palette:
-        if not all(0 <= value <= 255 for value in color):
-            raise ValueError(
-                "Invalid RGB values in palette. Each value must be in the range [0, 255]."
-            )
+    validate_image_content_type(image)
+    palette = parse_palette(palette_file)
 
     img_stream = io.BytesIO(image.read())
     img = cv2.imdecode(np.frombuffer(img_stream.read(), np.uint8), 1)
@@ -59,26 +89,14 @@ def upload_image():
     width = request.form.get("width", type=int)
     height = request.form.get("height", type=int)
 
-    if width and width <= 0:
-        raise ValueError("Width must be a positive value.")
-    if height and height <= 0:
-        raise ValueError("Height must be a positive value.")
+    if width is not None and width <= 0:
+        return jsonify(error="Width must be a positive value."), 400
+    if height is not None and height <= 0:
+        return jsonify(error="Height must be a positive value."), 400
 
-    original_height, original_width = img.shape[:2]
+    img = resize_image(img, width, height)
 
-    if width and not height:
-        aspect_ratio = original_height / original_width
-        height = int(aspect_ratio * width)
-
-    elif height and not width:
-        aspect_ratio = original_width / original_height
-        width = int(aspect_ratio * height)
-
-    if width and height:
-        img = cv2.resize(img, (width, height))
-
-    if width and height and (width <= 0 or height <= 0):
-        raise ValueError("Invalid dimensions after aspect ratio calculation.")
+    img = resize_image(img, width, height)
 
     p = palettum.Palettum(img, palette)
     result = p.convertToPalette()
@@ -88,5 +106,4 @@ def upload_image():
         raise ValueError("Failed to encode the processed image.")
 
     io_buf = io.BytesIO(buffer)
-
     return send_file(io_buf, mimetype="image/png")

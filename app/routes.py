8e1411305f76220fd5f540d89078bdfbc0b6c1f9
ArrowPC <battle.beater2005@gmail.com>
2023-09-22 04:57:19 +1000

    added better error handling

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
+from flask import jsonify, request, send_file
 from app import app
-from flask import request, send_file
 import cv2
 import palettum
 import io
 import numpy as np
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify(error=str(error)), 400
 
 
 @app.route("/")
@@ -21,11 +26,24 @@ def upload_image():
     image = request.files["image"]
     palette_file = request.files["palette"]
 
+    if not image.content_type.startswith("image/"):
+        raise ValueError("Uploaded file is not an image")
+
     palette_lines = palette_file.read().decode("utf-8").splitlines()
-    palette = [eval(color) for color in palette_lines]
+    try:
+        palette = [eval(color) for color in palette_lines]
+        if not all(isinstance(color, tuple) and len(color) == 3 for color in palette):
+            raise ValueError
+    except (SyntaxError, ValueError):
+        raise ValueError("Invalid palette format. Please provide a valid RGB palette.")
 
     img_stream = io.BytesIO(image.read())
     img = cv2.imdecode(np.frombuffer(img_stream.read(), np.uint8), 1)
+
+    if img is None:
+        raise ValueError(
+            "Failed to decode the image. Please provide a valid image format."
+        )
 
     width = request.form.get("width", type=int)
     height = request.form.get("height", type=int)
@@ -47,6 +65,9 @@ def upload_image():
     result = p.convertToPalette()
 
     is_success, buffer = cv2.imencode(".png", result)
+    if not is_success:
+        raise ValueError("Failed to encode the processed image.")
+
     io_buf = io.BytesIO(buffer)
 
     return send_file(io_buf, mimetype="image/png")

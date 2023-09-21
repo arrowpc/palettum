6ec8e9dc09e157aee647f7d6529aebc2da40d9f5
ArrowPC <battle.beater2005@gmail.com>
2023-09-22 00:22:14 +1000

    added MacOS caching, changed version to 0.0.4, made sure it builds wheels for 3.11 as well

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="palettum",
-    version="0.0.3",
+    version="0.0.4",
     author="ArrowPC",
     description="Core functionality for the Palettum project.",
     long_description="Core functionality for the Palettum project.",

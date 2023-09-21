f2c8d981edef406f49a25a3e1ba6ad1625b85bb1
ArrowPC <battle.beater2005@gmail.com>
2023-09-22 04:04:16 +1000

    refactored & adjusted tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 import io
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,33 +14,25 @@ def test_index(client):
 def test_upload_image_with_default_palette(client):
     image_path = os.path.join(current_dir, "test_images", "test.jpeg")
     palette_path = os.path.join(current_dir, "test_palettes", "default.txt")
-    with open(image_path, "rb") as img, open(palette_path, "r") as palette_file:
-        palette_content = palette_file.read().encode("utf-8")
-        response = client.post(
-            "/upload",
-            data={
-                "image": img,
-                "palette": (io.BytesIO(palette_content), "default.txt"),
-            },
-        )
+    with open(image_path, "rb") as img, open(palette_path, "rb") as palette:
+        response = client.post("/upload", data={"image": img, "palette": palette})
 
     assert response.status_code == 200
-    assert b"Image processed successfully!" in response.data
+
+    image = Image.open(io.BytesIO(response.data))
+    assert image.format == "PNG"
 
 
 def test_upload_image_with_resizing(client):
     image_path = os.path.join(current_dir, "test_images", "test.jpeg")
     palette_path = os.path.join(current_dir, "test_palettes", "default.txt")
-    with open(image_path, "rb") as img, open(palette_path, "r") as palette_file:
-        palette_content = palette_file.read().encode("utf-8")
+    with open(image_path, "rb") as img, open(palette_path, "rb") as palette:
         response = client.post(
-            "/upload",
-            data={
-                "image": img,
-                "palette": (io.BytesIO(palette_content), "default.txt"),
-                "width": 300,
-            },
+            "/upload", data={"image": img, "palette": palette, "width": 300}
         )
 
     assert response.status_code == 200
-    assert b"Image processed successfully!" in response.data
+
+    image = Image.open(io.BytesIO(response.data))
+    assert image.format == "PNG"
+    assert image.width == 300

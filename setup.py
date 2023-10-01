f2c9fbc48d9ec119df434cf6d81764129b9611b6
ArrowPC <battle.beater2005@gmail.com>
2023-10-02 10:59:16 +1100

    update publish.yml to support manylinux wheel building

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="palettum",
-    version="0.1.1",
+    version="0.1.2",
     author="ArrowPC",
     description="Core functionality for the Palettum project.",
     long_description="Core functionality for the Palettum project.",

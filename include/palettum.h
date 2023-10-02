c0b68dd82480ef11da2b8b8a9df8ade72e44428b
ArrowPC <battle.beater2005@gmail.com>
2023-10-03 10:47:47 +1100

    refactor to prepare for windows support

--- include/palettum.h
+++ include/palettum.h
@@ -11,6 +11,18 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+#ifndef M_PI
+#    define M_PI 3.14159265358979323846264338327950288
+#endif
+
+#ifndef M_PI_2
+#    define M_PI_2 1.57079632679489661923132169163975144
+#endif
+
+#ifndef M_PI_4
+#    define M_PI_4 0.785398163397448309615660845819875721
+#endif
+
 using namespace cv;
 using namespace std;
 namespace py = pybind11;

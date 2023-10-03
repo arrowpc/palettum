75e824ea6318f7529ecb3403f86fe744fe29d87a
ArrowPC <battle.beater2005@gmail.com>
2023-10-03 16:23:08 +1100

    go back to old arch checker w/ AMD64 added

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="palettum",
-    version="0.1.3",
+    version="0.1.4",
     author="ArrowPC",
     description="Core functionality for the Palettum project.",
     long_description="Core functionality for the Palettum project.",

84db15c7aa426f7a3d88879579df169a656c8ebc
arrowpc <battle.beater2005@gmail.com>
2024-11-17 12:24:37 +1100

    Remove opencv as a dependency

--- apps/core/include/palettum.h
+++ apps/core/include/palettum.h
@@ -8,11 +8,9 @@
 #include <atomic>
 #include <cmath>
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <vector>
 #include "processor.h"
 
-using namespace cv;
 using namespace std;
 namespace py = pybind11;
 
@@ -22,15 +20,12 @@ private:
     Image m_image;
     vector<Pixel> m_palette;
 
-    void mapToPalette(int startRow, int endRow, const Mat &img_lab,
-                      const std::vector<Lab> &lab_palette, Mat &result);
-
 public:
     Palettum(py::array_t<uint8_t> &image, const py::list &palette);
     static double py_deltaE(const py::list &lab1, const py::list &lab2);
     py::array_t<uint8_t> convertToPalette();
-    static bool validateImageColors(
-        const Mat &image, const std::vector<std::array<int, 3>> &palette);
+    //    static bool validateImageColors(
+    //        const Mat &image, const std::vector<std::array<int, 3>> &palette);
     static bool py_validateImageColors(
         pybind11::array_t<uint8_t> &image,
         const std::vector<std::array<int, 3>> &palette);

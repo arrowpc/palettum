c2c1324990666975baf12584d7122ff1301946b1
arrowpc <battle.beater2005@gmail.com>
2024-12-13 02:50:20 -0800

    Heavily reduce amount of intermediate variables

--- apps/core/src/color.cpp
+++ apps/core/src/color.cpp
@@ -1,4 +1,5 @@
 #include "color.h"
+#include "simd_utils.h"
 
 Lab::Lab(float L, float a, float b) noexcept
     : m_L(L)
@@ -97,38 +98,9 @@ static inline void hypotf_C(float *a, float *b, float *dst, int len)
     }
 };
 
-static inline void compute_g_C(float *cBar, float *g, float *onePlusG, int len)
-{
-#pragma omp simd
-    for (int i = 0; i < len; i++)
-    {
-        float result = cBar[i];
-        float x2 = result * result;
-        float x4 = x2 * x2;
-        float cBar7 = x4 * x2 * result;
-
-        float sqrt_val = std::sqrt(cBar7 / (cBar7 + pow25_7));
-        g[i] = 0.5f * (1.0f - sqrt_val);
-        onePlusG[i] = 1.0f + g[i];
-    }
-}
-
-static inline void compute_T_C(float *hBarPrime, float *T, int len)
-{
-#pragma omp simd
-    for (int i = 0; i < len; i++)
-    {
-        const float h = hBarPrime[i];
-        T[i] = 1.0f - (0.17f * cosf((h - 30.0f) * DEG_TO_RAD)) +
-               (0.24f * cosf((2.0f * h) * DEG_TO_RAD)) +
-               (0.32f * cosf((3.0f * h + 6.0f) * DEG_TO_RAD)) -
-               (0.20f * cosf((4.0f * h - 63.0f) * DEG_TO_RAD));
-    }
-}
-
 void Lab::deltaE(const Lab &ref, const Lab *comp, float *results, int len)
 {
-    const size_t totalFloats = len * 50;
+    const size_t totalFloats = len * 19;
 
     thread_local std::vector<float> workspace;
     if (workspace.size() < totalFloats)
@@ -142,50 +114,19 @@ void Lab::deltaE(const Lab &ref, const Lab *comp, float *results, int len)
     float *lBarPrime = comp_b + len;
     float *c2 = lBarPrime + len;
     float *cBar = c2 + len;
-    float *g = cBar + len;
-    float *onePlusG = g + len;
-    float *a1Prime = onePlusG + len;
+    float *a1Prime = cBar + len;
     float *a2Prime = a1Prime + len;
     float *c1Prime = a2Prime + len;
-    float *a1PrimeSq = c1Prime + len;
-    float *c2Prime = a1PrimeSq + len;
-    float *a2PrimeSq = c2Prime + len;
-    float *b2Sq = a2PrimeSq + len;
-    float *cBarPrime = b2Sq + len;
+    float *c2Prime = c1Prime + len;
+    float *cBarPrime = c2Prime + len;
     float *h1Prime = cBarPrime + len;
     float *h2Prime = h1Prime + len;
     float *deltaLPrime = h2Prime + len;
     float *deltaCPrime = deltaLPrime + len;
     float *deltahPrime = deltaCPrime + len;
-    float *c1Primec2Prime = deltahPrime + len;
-    float *sinDeltahPrime = c1Primec2Prime + len;
-    float *deltaHPrime = sinDeltahPrime + len;
-    float *lBarMinus50 = deltaHPrime + len;
-    float *lBarMinus50Sq = lBarMinus50 + len;
-    float *denominator = lBarMinus50Sq + len;
-    float *numerator = denominator + len;
-    float *fraction = numerator + len;
-    float *sL = fraction + len;
-    float *sC = sL + len;
-    float *hSum = sC + len;
-    float *hDiff = hSum + len;
-    float *hBarPrime = hDiff + len;
-    float *t = hBarPrime + len;
-    float *sH = t + len;
-    float *cBar7 = sH + len;
-    float *division = cBar7 + len;
-    float *expTerm = division + len;
-    float *negExpTerm = expTerm + len;
-    float *sinTerm = negExpTerm + len;
-    float *rT = sinTerm + len;
-    float *lightness = rT + len;
-    float *chroma = lightness + len;
-    float *hue = chroma + len;
-    float *lightnessSq = hue + len;
-    float *chromaSq = lightnessSq + len;
-    float *hueSq = chromaSq + len;
-    float *rTChroma = hueSq + len;
-    float *sum = rTChroma + len;
+    float *deltaHPrime = deltahPrime + len;
+    float *hBarPrime = deltaHPrime + len;
+    float *T = hBarPrime + len;
 
     const float ref_L = ref.L();
     const float ref_a = ref.a();
@@ -197,9 +138,8 @@ void Lab::deltaE(const Lab &ref, const Lab *comp, float *results, int len)
         comp_L[i] = comp[i].L();
         comp_a[i] = comp[i].a();
         comp_b[i] = comp[i].b();
+        lBarPrime[i] = (comp_L[i] + ref_L) * 0.5f;
     }
-
-    addmulcf_C(comp_L, ref_L, 0.5f, lBarPrime, len);
 
     const float c1 = sqrtf(ref_a * ref_a + ref_b * ref_b);
 
@@ -207,21 +147,22 @@ void Lab::deltaE(const Lab &ref, const Lab *comp, float *results, int len)
 
     addmulcf_C(c2, c1, 0.5f, cBar, len);
 
-    compute_g_C(cBar, g, onePlusG, len);
+#pragma omp simd
+    for (int i = 0; i < len; i++)
+    {
+        float cBar7 =
+            cBar[i] * cBar[i] * cBar[i] * cBar[i] * cBar[i] * cBar[i] * cBar[i];
+        float g = 1.0f + 0.5f * (1.0f - sqrtf(cBar7 / (cBar7 + pow25_7)));
+        a1Prime[i] = ref_a * g;
+        a2Prime[i] = comp_a[i] * g;
+    }
 
-    mulcf_C(onePlusG, ref_a, a1Prime, len);
-
-    mulf_C(comp_a, onePlusG, a2Prime, len);
-
-    mulf_C(a1Prime, a1Prime, a1PrimeSq, len);
-    float bSq = ref_b * ref_b;
-    addcf_C(a1PrimeSq, bSq, c1Prime, len);
-    sqrtf_C(c1Prime, c1Prime, len);
-
-    mulf_C(a2Prime, a2Prime, a2PrimeSq, len);
-    mulf_C(comp_b, comp_b, b2Sq, len);
-    addf_c(a2PrimeSq, b2Sq, c2Prime, len);
-    sqrtf_C(c2Prime, c2Prime, len);
+#pragma omp simd
+    for (int i = 0; i < len; i++)
+    {
+        c1Prime[i] = sqrtf(a1Prime[i] * a1Prime[i] + ref_b * ref_b);
+        c2Prime[i] = sqrtf(a2Prime[i] * a2Prime[i] + comp_b[i] * comp_b[i]);
+    }
 
     addf_c(c1Prime, c2Prime, cBarPrime, len);
     mulcf_C(cBarPrime, 0.5f, cBarPrime, len);
@@ -233,9 +174,7 @@ void Lab::deltaE(const Lab &ref, const Lab *comp, float *results, int len)
     muladdccf_C(h2Prime, RAD_TO_DEG, 360, h2Prime, len);
 
     addcf_C(comp_L, -ref_L, deltaLPrime, len);
-
     subf_c(c2Prime, c1Prime, deltaCPrime, len);
-
     subf_c(h2Prime, h1Prime, deltahPrime, len);
 
 #pragma omp simd
@@ -255,107 +194,70 @@ void Lab::deltaE(const Lab &ref, const Lab *comp, float *results, int len)
         }
     }
 
-    mulf_C(c1Prime, c2Prime, c1Primec2Prime, len);
-    sqrtf_C(c1Primec2Prime, c1Primec2Prime, len);
-
-    mulcf_C(deltahPrime, HALF_DEG_TO_RAD, sinDeltahPrime, len);
-    sinf_C(sinDeltahPrime, sinDeltahPrime, len);
-
-    mulf_C(c1Primec2Prime, sinDeltahPrime, deltaHPrime, len);
-    mulcf_C(deltaHPrime, 2, deltaHPrime, len);
-
-    addcf_C(lBarPrime, -50.0f, lBarMinus50, len);
-
-    mulf_C(lBarMinus50, lBarMinus50, lBarMinus50Sq, len);
-
-    addcf_C(lBarMinus50Sq, 20.0f, denominator, len);
-    sqrtf_C(denominator, denominator, len);
-
-    mulcf_C(lBarMinus50Sq, 0.015f, numerator, len);
-
-    divf_C(numerator, denominator, fraction, len);
-
-    addcf_C(fraction, 1.0f, sL, len);
-
-    muladdccf_C(cBarPrime, 0.045f, 1, sC, len);
-
-    addf_c(h1Prime, h2Prime, hSum, len);
-
-    subf_c(h1Prime, h2Prime, hDiff, len);
-
 #pragma omp simd
-    for (int i = 0; i < len; ++i)
+    for (int i = 0; i < len; i++)
     {
-        hDiff[i] = std::abs(hDiff[i]);
+        deltaHPrime[i] = 2.0f * sqrtf(c1Prime[i] * c2Prime[i]) *
+                         sinf(deltahPrime[i] * HALF_DEG_TO_RAD);
     }
 
 #pragma omp simd
     for (int i = 0; i < len; ++i)
     {
-        if (hDiff[i] <= 180)
+        if (std::abs(h1Prime[i] - h2Prime[i]) <= 180)
         {
-            hBarPrime[i] = hSum[i] / 2;
+            hBarPrime[i] = (h1Prime[i] + h2Prime[i]) / 2;
         }
-        else if (hSum[i] < 360)
+        else if (h1Prime[i] + h2Prime[i] < 360)
         {
-            hBarPrime[i] = (hSum[i] + 360) / 2;
+            hBarPrime[i] = (h1Prime[i] + h2Prime[i] + 360) / 2;
         }
         else
         {
-            hBarPrime[i] = (hSum[i] - 360) / 2;
+            hBarPrime[i] = (h1Prime[i] + h2Prime[i] - 360) / 2;
         }
     }
 
-    compute_T_C(hBarPrime, t, len);
+#pragma omp simd
+    for (int i = 0; i < len; i++)
+    {
+        T[i] = 1.0f - (0.17f * cosf((hBarPrime[i] - 30.0f) * DEG_TO_RAD)) +
+               (0.24f * cosf((2.0f * hBarPrime[i]) * DEG_TO_RAD)) +
+               (0.32f * cosf((3.0f * hBarPrime[i] + 6.0f) * DEG_TO_RAD)) -
+               (0.20f * cosf((4.0f * hBarPrime[i] - 63.0f) * DEG_TO_RAD));
+    }
 
-    mulcf_C(cBarPrime, 0.015f, sH, len);
-    muladdcf_C(sH, t, 1, sH, len);
-
-    copyf_C(cBarPrime, cBar7, len);
-    mulf_C(cBar7, cBar7, cBar7, len);
-    mulf_C(cBar7, cBar7, cBar7, len);
-    mulf_C(cBar7, cBarPrime, cBar7, len);
-    mulf_C(cBar7, cBarPrime, cBar7, len);
-    mulf_C(cBar7, cBarPrime, cBar7, len);
-
-    addcf_C(cBar7, pow25_7, denominator, len);
-    divf_C(cBar7, denominator, division, len);
-    sqrtf_C(division, division, len);
-
-    addcf_C(hBarPrime, -275.0f, expTerm, len);
-    mulcf_C(expTerm, 1 / 25.0f, expTerm, len);
-
-    mulf_C(expTerm, expTerm, negExpTerm, len);
-    mulcf_C(negExpTerm, -1.0f, negExpTerm, len);
-
-    expf_C(negExpTerm, negExpTerm, len);
-
-    mulcf_C(negExpTerm, 60.0f, negExpTerm, len);
-    mulcf_C(negExpTerm, DEG_TO_RAD, negExpTerm, len);
-
-    sinf_C(negExpTerm, sinTerm, len);
-    mulf_C(division, sinTerm, rT, len);
-    mulcf_C(rT, -2.0f, rT, len);
-
-    divf_C(deltaLPrime, sL, lightness, len);
-
-    divf_C(deltaCPrime, sC, chroma, len);
-
-    divf_C(deltaHPrime, sH, hue, len);
-
-    mulf_C(lightness, lightness, lightnessSq, len);
-    mulf_C(chroma, chroma, chromaSq, len);
-    mulf_C(hue, hue, hueSq, len);
-
-    mulf_C(rT, chroma, rTChroma, len);
-    mulf_C(rTChroma, hue, rTChroma, len);
-
-    copyf_C(lightnessSq, sum, len);
-    addf_c(sum, chromaSq, sum, len);
-    addf_c(sum, hueSq, sum, len);
-    addf_c(sum, rTChroma, sum, len);
-
-    sqrtf_C(sum, results, len);
+#pragma omp simd
+    for (int i = 0; i < len; ++i)
+    {
+        results[i] = sqrtf(
+            (deltaLPrime[i] /
+             (1.0f +
+              (0.015f * ((lBarPrime[i] - 50.0f) * (lBarPrime[i] - 50.0f)) /
+               sqrtf((lBarPrime[i] - 50.0f) * (lBarPrime[i] - 50.0f) +
+                     20.0f)))) *
+                (deltaLPrime[i] /
+                 (1.0f +
+                  (0.015f * ((lBarPrime[i] - 50.0f) * (lBarPrime[i] - 50.0f)) /
+                   sqrtf((lBarPrime[i] - 50.0f) * (lBarPrime[i] - 50.0f) +
+                         20.0f)))) +
+            (deltaCPrime[i] / (1.0f + 0.045f * cBarPrime[i])) *
+                (deltaCPrime[i] / (1.0f + 0.045f * cBarPrime[i])) +
+            (deltaHPrime[i] / (1.0f + 0.015f * cBarPrime[i] * T[i])) *
+                (deltaHPrime[i] / (1.0f + 0.015f * cBarPrime[i] * T[i])) +
+            (-2.0f *
+             sqrtf((cBarPrime[i] * cBarPrime[i] * cBarPrime[i] * cBarPrime[i] *
+                    cBarPrime[i] * cBarPrime[i] * cBarPrime[i]) /
+                   (cBarPrime[i] * cBarPrime[i] * cBarPrime[i] * cBarPrime[i] *
+                        cBarPrime[i] * cBarPrime[i] * cBarPrime[i] +
+                    pow25_7)) *
+             sinf(60.0f *
+                  expf(-((hBarPrime[i] - 275.0f) / 25.0f) *
+                       ((hBarPrime[i] - 275.0f) / 25.0f)) *
+                  DEG_TO_RAD)) *
+                (deltaCPrime[i] / (1.0f + 0.045f * cBarPrime[i])) *
+                (deltaHPrime[i] / (1.0f + 0.015f * cBarPrime[i] * T[i])));
+    }
 }
 
 float Lab::deltaE(const Lab &other) const noexcept

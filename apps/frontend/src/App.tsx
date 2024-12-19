ba5690d537de19b45c9185e0d2a18f2b6783b68a
arrowpc <battle.beater2005@gmail.com>
2024-12-19 05:45:58 -0800

    Implement ImageDemensions component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import ImageUpload from "./components/ImageUpload.tsx";
+import ImageDimensions from "./components/ImageDimensions.tsx";
 
 function App() {
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+
   return (
-    <div>
-      <ImageUpload />
+    <div className="max-w-xl mx-auto p-6">
+      <ImageUpload onFileSelect={setUploadedFile} />
+      <ImageDimensions file={uploadedFile} />
     </div>
   );
 }

7be0f704ce645ab7d60797d2b911277b1b3eafd3
arrowpc <battle.beater2005@gmail.com>
2024-12-21 23:20:15 -0800

    Implement PaletteManager
    dropdown menu with the ability to create and choose palettes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import ImageUpload from "./components/ImageUpload.tsx";
-import ImageDimensions from "./components/ImageDimensions.tsx";
+import ImageUpload from "./components/ImageUpload";
+import ImageDimensions from "./components/ImageDimensions";
+import PaletteManager from "./components/PaletteManager";
 
 function App() {
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
-
   return (
-    <div className="max-w-xl mx-auto p-6">
+    <div className="max-w-xl mx-auto p-6 space-y-6">
       <ImageUpload onFileSelect={setUploadedFile} />
       <ImageDimensions file={uploadedFile} />
+      <PaletteManager />
     </div>
   );
 }

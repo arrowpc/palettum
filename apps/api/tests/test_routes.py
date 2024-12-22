ed533c1cdc0425576cf7547b5b62b06efa61fdd1
arrowpc <battle.beater2005@gmail.com>
2024-12-22 03:46:25 -0800

    Add env examples and fix api testing

--- apps/api/tests/test_routes.py
+++ apps/api/tests/test_routes.py
@@ -1,23 +1,8 @@
 import os
-import sys
-
-from dotenv import load_dotenv
 
 import palettum
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
-
-load_dotenv(os.path.join(parent_dir, ".env"))
-
-
-def add_api_key(client, data=None, api_key=None):
-    if data is None:
-        data = {}
-    headers = {"X-API-Key": api_key or os.getenv("API_KEY")}
-    return headers
 
 
 def test_index(client):
@@ -34,10 +19,7 @@ def test_upload_image_with_default_palette(client):
             "image": (img, "test.jpeg", "image/jpeg"),
             "palette": (palette, "default.txt", "text/plain"),
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
 
     assert response.status_code == 200
     result_image = palettum.Image(memoryview(response.data))
@@ -55,55 +37,18 @@ def test_upload_image_with_resizing(client):
             "palette": (palette, "default.txt", "text/plain"),
             "width": "300",
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
 
     assert response.status_code == 200
     result_image = palettum.Image(memoryview(response.data))
     assert result_image.width() == 300
 
 
-def test_missing_api_key(client):
-    image_path = os.path.join(current_dir, "test_images", "test.jpeg")
-    palette_path = os.path.join(current_dir, "test_palettes", "default.txt")
-    with open(image_path, "rb") as img, open(palette_path, "rb") as palette:
-        data = {
-            "image": (img, "test.jpeg", "image/jpeg"),
-            "palette": (palette, "default.txt", "text/plain"),
-        }
-        response = client.post("/upload", data=data, content_type="multipart/form-data")
-
-    assert response.status_code == 401
-    assert b"No API key provided" in response.data
-
-
-def test_invalid_api_key(client):
-    image_path = os.path.join(current_dir, "test_images", "test.jpeg")
-    palette_path = os.path.join(current_dir, "test_palettes", "default.txt")
-    with open(image_path, "rb") as img, open(palette_path, "rb") as palette:
-        data = {
-            "image": (img, "test.jpeg", "image/jpeg"),
-            "palette": (palette, "default.txt", "text/plain"),
-        }
-        headers = {"X-API-Key": "incorrect_key"}
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
-
-    assert response.status_code == 401
-    assert b"Invalid API key" in response.data
-
-
 def test_missing_image(client):
     palette_path = os.path.join(current_dir, "test_palettes", "default.txt")
     with open(palette_path, "rb") as palette:
         data = {"palette": (palette, "default.txt", "text/plain")}
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 400
     assert b"No image provided" in response.data
 
@@ -112,10 +57,7 @@ def test_missing_palette(client):
     image_path = os.path.join(current_dir, "test_images", "test.jpeg")
     with open(image_path, "rb") as img:
         data = {"image": (img, "test.jpeg", "image/jpeg")}
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 400
     assert b"No palette provided" in response.data
 
@@ -128,10 +70,7 @@ def test_invalid_image_format(client):
             "image": (img, "invalid.txt", "text/plain"),
             "palette": (palette, "default.txt", "text/plain"),
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 400
     assert b"Uploaded file is not an image" in response.data
 
@@ -144,10 +83,7 @@ def test_invalid_palette_format(client):
             "image": (img, "test.jpeg", "image/jpeg"),
             "palette": (palette, "invalid.txt", "text/plain"),
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 400
     assert b"too many values to unpack" in response.data
 
@@ -161,10 +97,7 @@ def test_upload_image_with_resizing_height_only(client):
             "palette": (palette, "default.txt", "text/plain"),
             "height": "200",
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 200
     result_image = palettum.Image(memoryview(response.data))
     assert result_image.height() == 200
@@ -179,10 +112,7 @@ def test_negative_dimensions(client):
             "palette": (palette, "default.txt", "text/plain"),
             "width": "-300",
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 400
     assert b"Width must be a positive value" in response.data
 
@@ -195,9 +125,6 @@ def test_palette_not_in_range(client):
             "image": (img, "test.jpeg", "image/jpeg"),
             "palette": (palette, "range.txt", "text/plain"),
         }
-        headers = add_api_key(client)
-        response = client.post(
-            "/upload", data=data, content_type="multipart/form-data", headers=headers
-        )
+        response = client.post("/upload", data=data, content_type="multipart/form-data")
     assert response.status_code == 400
     assert b"incompatible constructor arguments" in response.data

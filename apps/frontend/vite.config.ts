92e6249868c71228ac28e6d149f8f29255e6ca6e
arrowpc <battle.beater2005@gmail.com>
2024-12-22 01:33:28 -0800

    Basic frontend -> api implementation

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -9,4 +9,24 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: (proxy, _options) => {
+          proxy.on("error", (err) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (_proxyReq, req) => {
+            console.log("Sending Request:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req) => {
+            console.log("Received Response:", proxyRes.statusCode, req.url);
+          });
+        },
+      },
+    },
+  },
 });

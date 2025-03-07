6bdb9a214cedac8ac47374a7cc8744c11a3d3a86
arrowpc <battle.beater2005@gmail.com>
2025-03-08 07:18:12 +1100

    v0.3.2
    fp32 for aarch64 is good enough (pun not intended)

--- apps/core/setup.py
+++ apps/core/setup.py
@@ -158,7 +158,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="palettum",
-    version="0.3.1",
+    version="0.3.2",
     author="ArrowPC",
     description="Core functionality for the Palettum project.",
     long_description="Core functionality for the Palettum project.",

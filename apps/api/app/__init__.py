03492d297de80ef04747bb243746e3d415162a60
arrowpc <battle.beater2005@gmail.com>
2025-03-10 16:14:07 +1100

    Improve api logging

--- apps/api/app/__init__.py
+++ apps/api/app/__init__.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import sys
 
 from dotenv import load_dotenv
 from flask import Flask
@@ -15,5 +17,17 @@ else:
     CORS(app)
 
 app.config["ENV"] = os.getenv("FLASK_ENV", "production")
+
+
+is_gunicorn = "gunicorn" in sys.modules
+
+if is_gunicorn:
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+else:
+    default_logger = logging.getLogger(__name__)
+    app.logger.handlers = default_logger.handlers
+    app.logger.setLevel(default_logger.level)
 
 from app import routes

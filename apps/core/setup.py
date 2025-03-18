b5060e2561a72591c1108572f0f95ab841702408
arrowpc <battle.beater2005@gmail.com>
2025-03-18 14:59:51 +1100

    v0.3.3

--- apps/core/setup.py
+++ apps/core/setup.py
@@ -125,7 +125,7 @@ class CMakeBuild(build_ext):
                 cmake_args += [
                     f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
                 ]
-                build_args += ["--config", cfg]
+                build_args += ["--config", cfg, "--parallel"]
 
         if sys.platform.startswith("darwin"):
             # Cross-compile support for macOS - respect ARCHFLAGS if set
@@ -158,7 +158,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="palettum",
-    version="0.3.2",
+    version="0.3.3",
     author="ArrowPC",
     description="Core functionality for the Palettum project.",
     long_description="Core functionality for the Palettum project.",

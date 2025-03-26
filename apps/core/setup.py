b4eef1566b69f804aa75c649f38eb2cf600c5908
arrowpc <battle.beater2005@gmail.com>
2025-03-26 20:12:27 +1100

    v0.3.9

--- apps/core/setup.py
+++ apps/core/setup.py
@@ -159,7 +159,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="palettum",
-    version="0.3.8",
+    version="0.3.9",
     author="ArrowPC",
     description="Core functionality for the Palettum project.",
     long_description="Core functionality for the Palettum project.",

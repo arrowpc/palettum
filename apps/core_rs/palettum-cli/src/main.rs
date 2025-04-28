646fe4a2513532ee00033b71320f7806cf341f25
arrowpc <battle.beater2005@gmail.com>
2025-04-28 13:23:08 +1000

    core_rs: Make TUI off by default

--- apps/core_rs/palettum-cli/src/main.rs
+++ apps/core_rs/palettum-cli/src/main.rs
@@ -3,7 +3,10 @@ use clap::Parser;
 use palettum_cli::cli::args::Cli;
 
 #[cfg(feature = "tui")]
-use palettum_cli::tui::run_tui;
+use {
+    console::style,
+    palettum_cli::tui::run_tui,
+};
 
 use palettum_cli::cli::runner::run_cli;
 
@@ -12,6 +15,12 @@ fn main() -> Result<()> {
 
     #[cfg(feature = "tui")]
     if cli.interactive || cli.command.is_none() {
+        eprintln!(
+            "{}",
+            style("Warning: the TUI is experimental, lacking most features, and may not work as expected.")
+                .yellow()
+                .bold()
+        );
         run_tui(cli.command.map(|c| c.into()))?;
         return Ok(());
     }

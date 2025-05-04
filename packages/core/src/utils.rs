615382d56d7f6d02f622132c3a1b2ba95edcf3f8
arrowpc <battle.beater2005@gmail.com>
2025-05-04 11:42:41 +1000

    core: Create *extremely* basic tests

--- packages/core/src/utils.rs
+++ packages/core/src/utils.rs
@@ -1,40 +1,6 @@
-use crate::config::Config;
 use crate::image::Image;
 use image::Rgba;
-use image::{Rgb, RgbaImage};
 use std::collections::HashMap;
-use std::collections::HashSet;
-
-// This function should probably be moved to tests, as I don't see much of a usecase outside of
-// testing.
-// Also should use local Image instead of RgbaImage
-pub fn validate_image(image: &RgbaImage, config: &Config) -> bool {
-    log::debug!("Validating image against palette...");
-    let palette_lookup: HashSet<Rgb<u8>> = config.palette.iter().cloned().collect();
-    let height = image.height();
-    let width = image.width();
-
-    for y in 0..height {
-        for x in 0..width {
-            let current_pixel = image.get_pixel(x, y);
-
-            if current_pixel.0[3] >= config.transparency_threshold {
-                let pixel_rgb = Rgb([current_pixel.0[0], current_pixel.0[1], current_pixel.0[2]]);
-                if !palette_lookup.contains(&pixel_rgb) {
-                    log::debug!(
-                        "Validation failed: Pixel {:?} at ({}, {}) not in palette.",
-                        pixel_rgb,
-                        x,
-                        y
-                    );
-                    return false;
-                }
-            }
-        }
-    }
-    log::debug!("Image validation successful.");
-    return true;
-}
 
 pub fn resize_image_if_needed(
     image: &Image,

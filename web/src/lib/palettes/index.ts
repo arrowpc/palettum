e89a8ad3994af80d295cd205c69cf8a441c35811
arrowpc <battle.beater2005@gmail.com>
2025-05-05 15:33:46 +1000

    Overhaul palette format

--- web/src/lib/palettes/index.ts
+++ web/src/lib/palettes/index.ts
@@ -13,23 +13,3 @@ export type { Color, Palette };
 export { LIMITS, DEFAULTS };
 
 export { rgbToHex, hexToRgb, normalizeColor, isSameColor, validatePalette };
-
-import { gruvbox } from "./palettes/gruvbox";
-import { rplace_22 } from "./palettes/rplace_22";
-import { catppuccin_latte } from "./palettes/catppuccin-latte";
-import { catppuccin_frappe } from "./palettes/catppuccin-frappe";
-import { catppuccin_macchiato } from "./palettes/catppuccin-macchiato";
-import { catppuccin_mocha } from "./palettes/catppuccin-mocha";
-import { dracula } from "./palettes/dracula";
-import { nord } from "./palettes/nord";
-
-export const defaultPalettes = [
-  gruvbox,
-  rplace_22,
-  catppuccin_mocha,
-  catppuccin_latte,
-  catppuccin_frappe,
-  catppuccin_macchiato,
-  dracula,
-  nord,
-] as const;

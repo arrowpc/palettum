d3191546264f888d58d7d9fc677884de06af255f
omar <69222225+arrowpc@users.noreply.github.com>
2025-05-26 21:22:01 +1000

    cli: Optional profiling support (#52)
    * (No windows support)

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod cli;
 pub mod logger;
+#[cfg(all(feature = "profiler", windows))]
+pub mod profiler;
 pub mod style;
---
+++ cli/src/profiler.rs
@@ -0,0 +1,53 @@
+#![cfg(all(feature = "profiler", not(windows)))]
+use palettum::error::Result;
+
+pub fn start_profiling() -> Option<pprof::ProfilerGuard<'static>> {
+    match pprof::ProfilerGuardBuilder::default()
+        .frequency(1000)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
+        .build()
+    {
+        Ok(guard) => Some(guard),
+        Err(e) => {
+            log::error!("failed to build profiler guard: {e}");
+            None
+        }
+    }
+}
+
+pub fn finish_profiling(profiler_guard: Option<pprof::ProfilerGuard>) -> Result<()> {
+    match profiler_guard
+        .expect("failed to retrieve profiler guard")
+        .report()
+        .build()
+    {
+        Ok(report) => {
+            #[cfg(feature = "profiler-flamegraph")]
+            {
+                use chrono::Local;
+                use std::fs::File;
+
+                let now = Local::now();
+                let datetime = now.format("%Y-%m-%d_%H-%M-%S");
+                let file = File::create(format!(
+                    "{}_{}.flamegraph.svg",
+                    env!("CARGO_PKG_NAME"),
+                    datetime
+                ))?;
+                if let Err(e) = report.flamegraph(file) {
+                    log::error!("failed to create flamegraph file: {e}");
+                }
+            }
+
+            #[cfg(not(feature = "profiler-flamegraph"))]
+            {
+                log::info!("profiling report: {:?}", &report);
+            }
+        }
+        Err(e) => {
+            log::error!("failed to build profiler report: {e}");
+        }
+    }
+
+    Ok(())
+}

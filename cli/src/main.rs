35e25c6ecdaf3b22fdfcb07918dcdd1c74302f9f
omar <69222225+arrowpc@users.noreply.github.com>
2025-05-26 21:22:01 +1000

    cli: Optional profiling support (#52)
    * (No windows support)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,25 +1,42 @@
 use clap::Parser;
 use cli::cli::args::Cli;
-
 use cli::{cli::runner::run_cli, logger};
 use indicatif::MultiProgress;
 use palettum::error::Result;
 use std::{env, process};
 
+#[cfg(all(feature = "profiler", not(windows)))]
+mod profiler;
+#[cfg(all(feature = "profiler", not(windows)))]
+use pprof::ProfilerGuard;
+#[cfg(all(feature = "profiler", not(windows)))]
+type MyProfilerGuard<'a> = ProfilerGuard<'a>;
+
+#[cfg(not(all(feature = "profiler", not(windows))))]
+type MyProfilerGuard<'a> = ();
+
 fn main() -> Result<()> {
     let args = Cli::parse();
 
     if args.verbose == 1 {
-        unsafe { env::set_var("RUST_LOG", "debug") };
+        env::set_var("RUST_LOG", "debug");
     } else if args.verbose > 1 {
-        unsafe { env::set_var("RUST_LOG", "trace") };
+        env::set_var("RUST_LOG", "trace");
     } else if env::var_os("RUST_LOG").is_none() {
-        unsafe { env::set_var("RUST_LOG", "info") };
+        env::set_var("RUST_LOG", "info");
     }
 
     let multi = MultiProgress::new();
     let level = logger::init(multi.clone())?;
     log::set_max_level(level);
+
+    let mut _profiler_guard: Option<MyProfilerGuard<'_>> = None;
+
+    #[cfg(all(feature = "profiler", not(windows)))]
+    {
+        // Adjust this path if your profiler module is elsewhere!
+        _profiler_guard = profiler::start_profiling();
+    }
 
     let exit_code = match run_cli(args, multi) {
         Ok(()) => 0,
@@ -28,6 +45,11 @@ fn main() -> Result<()> {
             1
         }
     };
+
+    #[cfg(all(feature = "profiler", not(windows)))]
+    {
+        profiler::finish_profiling(_profiler_guard)?;
+    }
 
     process::exit(exit_code);
 }

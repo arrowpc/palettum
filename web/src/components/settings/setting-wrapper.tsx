21dca960251d7abd31257230b146041d57dc1d77
arrowpc <battle.beater2005@gmail.com>
2025-07-17 17:11:21 +1000

    moar!!

--- web/src/components/settings/setting-wrapper.tsx
+++ web/src/components/settings/setting-wrapper.tsx
@@ -2,14 +2,22 @@ import React from "react";
 
 interface SettingItemWrapperProps {
   label: string;
-  control?: React.ReactNode; 
+  control?: React.ReactNode;
   children: React.ReactNode;
 }
 
-export default function SettingWrapper({ label, control, children }: SettingItemWrapperProps) {
+export default function SettingWrapper({
+  label,
+  control,
+  children,
+}: SettingItemWrapperProps) {
   return (
-    <div className="flex flex-col gap-5 items-center px-4">
-      <div className="flex flex-row items-center justify-center w-full gap-8">
+    <div className="flex flex-col gap-5 items-center">
+      <div
+        className={`flex flex-row items-center w-full ${
+          control ? "justify-between" : "justify-center"
+        }`}
+      >
         <label className="text-lg">{label}</label>
         {control}
       </div>
